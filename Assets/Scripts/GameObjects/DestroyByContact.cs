fbeddefc5e22a0bbbb5b6e91a24016677c75318b
Brandon Jones <bcjones1@fullsail.edu>
2017-06-02 09:00:00 -0400

    Initial Commit

---
+++ Assets/Scripts/GameObjects/DestroyByContact.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        if (other.tag == "Player")
+        {
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+
+    }
+}
